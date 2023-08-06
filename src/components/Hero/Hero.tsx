a812ab85ea84c32e89d9f9105642b57397de04b1
Salih Zorer <zorer.salih@gmail.com>
2023-08-06 07:20:05 +0300

    fix: responsiveness updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,25 +6,31 @@ function Hero() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className="flex flex-wrap gap-y-8 gap-x-8 justify-evenly items-center h-full min-h-[400px] sm:px-32 xl:px-40  mt-6 ">
+    <div className="flex flex-wrap gap-y-8 gap-x-8 justify-evenly items-center h-full min-h-[400px] px-7 sm:px-32 lg:px-34 xl:px-40  mt-6 ">
       <div className="flex flex-col gap-4 min-w-min text-center xl:text-start ">
         <h2
-          className={`${theme.primaryText} text-4xl font-semibold align-bottom`}
+          className={`${theme.primaryText} text-3xl md:text-4xl font-semibold align-bottom`}
         >
           Hi there
-          <div className=" ml-4 pr-4 inline-block text-3xl animate-wave origin-bottom">
+          <div className=" ml-4 pr-4 inline-block text-2xl md:text-3xl animate-wave origin-bottom">
             👋
           </div>
         </h2>
-        <h1 className={`${theme.headingText} font-semibold  text-5xl`}>
+        <h1
+          className={`${theme.headingText} font-semibold text-4xl md:text-5xl`}
+        >
           I'm{" "}
           <span className={`${theme.headingText} font-bold`}>Salih Zorer</span>
         </h1>
-        <h2 className={`${theme.link} font-semibold text-4xl`}>
+        <h2 className={`${theme.link} font-semibold text-3xl md:text-4xl`}>
           Frontend Developer
         </h2>
       </div>
-      <img src={hero} alt="hero" className=" w-2/5 min-w-[410px]" />
+      <img
+        src={hero}
+        alt="hero"
+        className=" w-full md:w-2/5 sm:min-w-[410px]"
+      />
     </div>
   );
 }
