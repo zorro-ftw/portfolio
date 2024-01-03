f8617fbb7aee606564073f5e65613310e932f102
Salih Zorer <zorer.salih@gmail.com>
2024-01-03 23:49:59 +0300

    fix: warnings

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,6 +1,4 @@
-import { Footer } from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
-import { ProjectsGrid } from "../../components/ProjectsGrid/ProjectsGrid";
 
 import { UnderConstruction } from "../../components/UnderConstruction/UnderConstruction";
 
